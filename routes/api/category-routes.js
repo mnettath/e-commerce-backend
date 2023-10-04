8c7e04393b094528efdadcdc76300897f053ca36
Maya N <nettathm@gmail.com>
2023-10-03 19:21:59 -0600

    second GET route in category-routes now works properly

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -11,12 +11,27 @@ router.get("/", async (req, res) => {
     });
     res.status(200).json(categoryData);
   } catch (err) {
-    console.log(err);
     res.status(500).json(err);
   }
 });
 
-router.get("/:id", (req, res) => {
+// http://localhost:3001/api/categories/:id
+router.get("/:id", async (req, res) => {
+  try {
+    const categoryData = await Category.findByPk(req.params.id, {
+      include: [{ model: Product }],
+    });
+
+    if (!categoryData) {
+      res.status(404).json({ message: "No category found with this id!" });
+      return;
+    }
+
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+
   // find one category by its `id` value
   // be sure to include its associated Products
 });
