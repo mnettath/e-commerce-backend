dd4e11b3997c0e739af184c198d84aa8fbcf784d
Maya N <nettathm@gmail.com>
2023-10-03 19:57:52 -0600

    worked on PUT and DELETE for category-routes, has bugs

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -37,19 +37,48 @@ router.get("/:id", async (req, res) => {
 router.post("/", async (req, res) => {
   try {
     const newCategory = await Category.create(req.body);
-    res.status(200).json({ message: "Category has been added!", newCategory });
+    res.status(200).json(newCategory);
   } catch (err) {
     res.status(400).json(err);
   }
-  // create a new category
 });
 
-router.put("/:id", (req, res) => {
+// http://localhost:3001/api/categories/:id
+router.put("/:id", async (req, res) => {
+  try {
+    const updatedCategory = await Category.update(
+      {
+        category: req.body.category_name,
+      },
+      {
+        where: {
+          id: req.params.id,
+        },
+      }
+    );
+    res.status(200).json(updatedCategory);
+  } catch {
+    res.status(400).json(err);
+  }
+  // returns [0] and does not update the category
+
   // update a category by its `id` value
 });
 
-router.delete("/:id", (req, res) => {
+router.delete("/:id", async (req, res) => {
+  try {
+    const deletedCategory = await Category.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.json(deletedCategory);
+  } catch (err) {
+    res.status(400).json(err);
+  }
   // delete a category by its `id` value
 });
+// does not delete item,
+// "sqlMessage": "Cannot delete or update a parent row: a foreign key constraint fails (`ecommerce_db`.`product`, CONSTRAINT `product_ibfk_1` FOREIGN KEY (`category_id`) REFERENCES `category` (`id`) ON UPDATE CASCADE)"
 
 module.exports = router;
